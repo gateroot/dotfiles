7f4e5834dff86f790890505ad7c7fcac2aaed30e
sekine-hiroaki <sekine@cloud-ace.jp>
2018-07-03 12:52:10 +0900

    Mod. add command to create symbolic link for fish

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@
 ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
