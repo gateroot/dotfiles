0993c0c10d2e6cadb971d41f8b51556111e342cf
sekine-hiroaki <sekine@cloud-ace.jp>
2018-07-03 13:16:59 +0900

    Mod. change file paths and add confirmation before execution of each commands

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/.dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -si ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -si ~/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -si ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
