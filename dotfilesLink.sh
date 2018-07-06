117c8868c5b00b74d54ba72fcb0d0a1729c3c089
sekine-hiroaki <sekine@cloud-ace.jp>
2018-07-06 14:51:36 +0900

    Mod. Add TOML files and move plugins

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -si ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -si ~/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -si ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -si ~/dotfiles/.config/nvim/dein.toml ~/.config/nvim/dein.toml
+ln -si ~/dotfile/.config/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
